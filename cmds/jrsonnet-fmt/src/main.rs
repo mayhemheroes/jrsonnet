80edc06977064874b4ca91bed7685b3b776f64a9
Yaroslav Bolyukin <iam@lach.pw>
2022-06-20 14:21:24 +0200

    refactor: parser and grammar compatibility
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- cmds/jrsonnet-fmt/src/main.rs
+++ cmds/jrsonnet-fmt/src/main.rs
@@ -1,9 +1,14 @@
-use std::path::PathBuf;
+use std::any::type_name;
 
 use dprint_core::formatting::{PrintItems, PrintOptions, Signal};
-use jrsonnet_parser::{
-	ArgsDesc, BinaryOpType, BindSpec, Expr, FieldName, LocExpr, Member, ObjBody, Param, ParamsDesc,
-	ParserSettings, Visibility,
+use jrsonnet_rowan_parser::{
+	nodes::{
+		ArgsDesc, Assertion, BinaryOperator, Bind, CompSpec, Destruct, DestructArrayPart,
+		DestructRest, Expr, Field, FieldName, ForSpec, IfSpec, ImportKind, LhsExpr, Literal,
+		Member, Name, Number, ObjBody, ObjLocal, ParamsDesc, SliceDesc, SourceFile, String,
+		UnaryOperator,
+	},
+	AstToken, SyntaxToken,
 };
 
 pub trait Printable {
@@ -12,11 +17,12 @@ pub trait Printable {
 
 macro_rules! pi {
 	(@i; $($t:tt)*) => {{
+		#[allow(unused_mut)]
 		let mut o = PrintItems::new();
 		pi!(@s; o: $($t)*);
 		o
 	}};
-	(@s; $o:ident: str($e:expr) $($t:tt)*) => {{
+	(@s; $o:ident: str($e:expr $(,)?) $($t:tt)*) => {{
 		$o.push_str($e);
 		pi!(@s; $o: $($t)*);
 	}};
@@ -61,250 +67,370 @@ macro_rules! p {
 	};
 }
 
-impl Printable for FieldName {
-	fn print(&self) -> PrintItems {
-		match self {
-			FieldName::Fixed(f) => {
-				p!(new: str(f))
-			}
-			FieldName::Dyn(_) => todo!(),
-		}
-	}
-}
-
-impl Printable for Visibility {
-	fn print(&self) -> PrintItems {
-		match self {
-			Visibility::Normal => p!(new: str(":")),
-			Visibility::Hidden => p!(new: str("::")),
-			Visibility::Unhide => p!(new: str(":::")),
-		}
-	}
-}
-
-impl Printable for BinaryOpType {
-	fn print(&self) -> PrintItems {
-		let o = self.to_string();
-		p!(new: str(&o))
-	}
-}
-
-impl<T: Printable> Printable for Option<T> {
+impl<P> Printable for Option<P>
+where
+	P: Printable,
+{
 	fn print(&self) -> PrintItems {
 		if let Some(v) = self {
 			v.print()
 		} else {
-			PrintItems::new()
+			p!(new: str(
+				&format!(
+					"/*missing {}*/",
+					type_name::<P>().replace("jrsonnet_rowan_parser::generated::nodes::", "")
+				),
+			))
 		}
 	}
 }
 
-impl Printable for Param {
+impl Printable for SyntaxToken {
 	fn print(&self) -> PrintItems {
-		p!(new:
-			str(&self.0)
-			if(self.1.is_some())(str(" = ") {self.1})
-		)
+		p!(new: str(&self.to_string()))
+	}
+}
+
+impl Printable for String {
+	fn print(&self) -> PrintItems {
+		p!(new: str(&format!("{}", self)))
+	}
+}
+impl Printable for Number {
+	fn print(&self) -> PrintItems {
+		p!(new: str(&format!("{}", self)))
+	}
+}
+
+impl Printable for Name {
+	fn print(&self) -> PrintItems {
+		p!(new: {self.ident_lit()})
+	}
+}
+
+impl Printable for DestructRest {
+	fn print(&self) -> PrintItems {
+		let mut pi = p!(new: str("..."));
+		if let Some(name) = self.into() {
+			p!(pi: {name});
+		}
+		pi
+	}
+}
+
+impl Printable for Destruct {
+	fn print(&self) -> PrintItems {
+		let mut pi = p!(new:);
+		match self {
+			Destruct::DestructFull(f) => {
+				p!(pi: {f.name()})
+			}
+			Destruct::DestructSkip(_) => p!(pi: str("?")),
+			Destruct::DestructArray(a) => {
+				p!(pi: str("[") >i nl);
+				for el in a.destruct_array_parts() {
+					match el {
+						DestructArrayPart::DestructArrayElement(e) => {
+							p!(pi: {e.destruct()} str(",") nl)
+						}
+						DestructArrayPart::DestructRest(d) => {
+							p!(pi: {d} str(",") nl)
+						}
+					}
+				}
+				p!(pi: <i str("]"));
+			}
+			Destruct::DestructObject(o) => {
+				p!(pi: str("{") >i nl);
+				for item in o.destruct_object_fields() {
+					p!(pi: {item.field()});
+					if let Some(des) = item.destruct() {
+						p!(pi: str(": ") {des})
+					}
+					if let Some(def) = item.expr() {
+						p!(pi: str(" = ") {def});
+					}
+					p!(pi: str(",") nl);
+				}
+				if let Some(rest) = o.destruct_rest() {
+					p!(pi: {rest} nl)
+				}
+				p!(pi: <i str("}"));
+			}
+		}
+		pi
+	}
+}
+
+impl Printable for FieldName {
+	fn print(&self) -> PrintItems {
+		match self {
+			FieldName::FieldNameFixed(f) => {
+				if let Some(id) = f.id() {
+					p!(new: {id})
+				} else if let Some(str) = f.string() {
+					p!(new: {str})
+				} else {
+					p!(new: str("/*missing FieldName*/"))
+				}
+			}
+			FieldName::FieldNameDynamic(d) => {
+				p!(new: str("[") {d.expr()} str("]"))
+			}
+		}
+	}
+}
+impl Printable for Field {
+	fn print(&self) -> PrintItems {
+		let mut pi = p!(new:);
+		match self {
+			Field::FieldNormal(n) => {
+				p!(pi: {n.field_name()});
+				if n.plus_token().is_some() {
+					p!(pi: str("+"));
+				}
+				p!(pi: str(": ") {n.expr()});
+			}
+			Field::FieldMethod(m) => {
+				p!(pi: {m.field_name()} {m.params_desc()} str(": ") {m.expr()});
+			}
+		}
+		pi
+	}
+}
+
+impl Printable for ObjLocal {
+	fn print(&self) -> PrintItems {
+		p!(new: str("local ") {self.bind()})
+	}
+}
+
+impl Printable for Assertion {
+	fn print(&self) -> PrintItems {
+		let mut pi = p!(new: str("assert ") {self.condition()});
+		if self.colon_token().is_some() || self.message().is_some() {
+			p!(pi: str(": ") {self.message()})
+		}
+		pi
 	}
 }
 
 impl Printable for ParamsDesc {
 	fn print(&self) -> PrintItems {
-		let mut out = PrintItems::new();
-		for (i, item) in self.0.iter().enumerate() {
-			if i != 0 {
-				p!(out: str(", "));
+		let mut pi = p!(new: str("(") >i nl);
+		for param in self.params() {
+			p!(pi: {param.destruct()});
+			if param.assign_token().is_some() || param.expr().is_some() {
+				p!(pi: str(" = ") {param.expr()})
 			}
-			out.extend(item.print());
+			p!(pi: str(",") nl)
 		}
-		out
+		p!(pi: <i str(")"));
+		pi
 	}
 }
-
 impl Printable for ArgsDesc {
 	fn print(&self) -> PrintItems {
-		let mut out = PrintItems::new();
-		let mut first = Some(());
-		for u in self.unnamed.iter() {
-			if first.take().is_none() {
-				p!(out: str(", "));
+		let mut pi = p!(new: str("(") >i nl);
+		for arg in self.args() {
+			if arg.name().is_some() || arg.assign_token().is_some() {
+				p!(pi: {arg.name()} str(" = "));
 			}
-			p!(out: {u})
+			p!(pi: {arg.expr()} str(",") nl)
 		}
-		for (n, u) in self.named.iter() {
-			if first.take().is_none() {
-				p!(out: str(", "));
-			}
-			p!(out: str(n) str(" = ") {u})
-		}
-
-		out
+		p!(pi: <i str(")"));
+		pi
 	}
 }
-
-impl Printable for BindSpec {
+impl Printable for SliceDesc {
 	fn print(&self) -> PrintItems {
-		p!(new: str(&self.name) if(self.params.is_some())(str("(") {self.params} str(")")) str(" = ") {self.value})
-	}
-}
-
-struct StrExpr<'s>(&'s str);
-
-impl<'s> Printable for StrExpr<'s> {
-	fn print(&self) -> PrintItems {
-		todo!()
+		let mut pi = p!(new: str("["));
+		if self.from().is_some() {
+			p!(pi: {self.from()});
+		}
+		p!(pi: str(":"));
+		if self.end().is_some() {
+			p!(pi: {self.end().map(|e|e.expr())})
+		}
+		// Keep only one : in case if we don't need step
+		if self.step().is_some() {
+			p!(pi: str(":") {self.step().map(|e|e.expr())});
+		}
+		p!(pi: str("]"));
+		pi
 	}
 }
 
 impl Printable for ObjBody {
 	fn print(&self) -> PrintItems {
-		let mut pi = PrintItems::new();
-		p!(pi: str("{"));
 		match self {
-			ObjBody::MemberList(m) => {
-				if !m.is_empty() {
-					p!(pi: nl > i);
-					for m in m {
-						match m {
-							Member::Field(f) => {
-								p!(pi:
-									{f.name} {f.params}
-									if(f.plus)(str("+"))
-									{f.visibility} str(" ")
-									{f.value}
-									str(",") nl
-								);
-							}
-							Member::BindStmt(s) => {
-								p!(pi: str("local ") {s} str(",") nl)
-							}
-							Member::AssertStmt(a) => p!(pi: str("assert ") {a.0} if(a.1.is_some())(
-								str(" : ") {a.1}
-							) str(",") nl),
+			ObjBody::ObjBodyComp(_) => todo!(),
+			ObjBody::ObjBodyMemberList(l) => {
+				let mut pi = p!(new:);
+				for mem in l.members() {
+					match mem {
+						Member::MemberBindStmt(b) => {
+							p!(pi: {b.obj_local()})
+						}
+						Member::MemberAssertStmt(ass) => {
+							p!(pi: {ass.assertion()})
+						}
+						Member::MemberField(f) => {
+							p!(pi: {f.field()})
 						}
 					}
-					p!(pi: <i);
-				} else {
-					p!(pi: str(" "))
+					p!(pi: str(",") nl)
 				}
+				pi
 			}
-			ObjBody::ObjComp(_) => todo!(),
 		}
-		p!(pi: str("}"));
-		pi
 	}
 }
-
+impl Printable for UnaryOperator {
+	fn print(&self) -> PrintItems {
+		p!(new: str(self.text()))
+	}
+}
+impl Printable for BinaryOperator {
+	fn print(&self) -> PrintItems {
+		p!(new: str(self.text()))
+	}
+}
+impl Printable for Bind {
+	fn print(&self) -> PrintItems {
+		match self {
+			Bind::BindDestruct(d) => {
+				p!(new: {d.into()} str(" = ") {d.value()})
+			}
+			Bind::BindFunction(f) => {
+				p!(new: str("function") {f.params()} str(" = ") {f.value()})
+			}
+		}
+	}
+}
+impl Printable for Literal {
+	fn print(&self) -> PrintItems {
+		p!(new: str(&self.syntax().to_string()))
+	}
+}
+impl Printable for ImportKind {
+	fn print(&self) -> PrintItems {
+		p!(new: str(&self.syntax().to_string()))
+	}
+}
+impl Printable for LhsExpr {
+	fn print(&self) -> PrintItems {
+		p!(new: {self.expr()})
+	}
+}
+impl Printable for ForSpec {
+	fn print(&self) -> PrintItems {
+		p!(new: str("for ") {self.bind()} str(" in ") {self.expr()})
+	}
+}
+impl Printable for IfSpec {
+	fn print(&self) -> PrintItems {
+		p!(new: str("if ") {self.expr()})
+	}
+}
+impl Printable for CompSpec {
+	fn print(&self) -> PrintItems {
+		match self {
+			CompSpec::ForSpec(f) => f.print(),
+			CompSpec::IfSpec(i) => i.print(),
+		}
+	}
+}
 impl Printable for Expr {
 	fn print(&self) -> PrintItems {
-		let mut pi = PrintItems::new();
 		match self {
-			Expr::Literal(l) => match l {
-				jrsonnet_parser::LiteralType::This => p!(pi: str("self")),
-				jrsonnet_parser::LiteralType::Super => p!(pi: str("super")),
-				jrsonnet_parser::LiteralType::Dollar => p!(pi: str("$")),
-				jrsonnet_parser::LiteralType::Null => p!(pi: str("null")),
-				jrsonnet_parser::LiteralType::True => p!(pi: str("true")),
-				jrsonnet_parser::LiteralType::False => p!(pi: str("false")),
-			},
-			Expr::Str(s) => {
-				p!(pi: str("\"") str(s) str("\""))
+			Expr::ExprBinary(b) => {
+				p!(new: {b.lhs()} str(" ") {b.binary_operator()} str(" ") {b.rhs()})
 			}
-			Expr::Num(n) => {
-				let n = n.to_string();
-				p!(pi: str(&n));
+			Expr::ExprUnary(u) => p!(new: {u.unary_operator()} {u.rhs()}),
+			Expr::ExprSlice(s) => {
+				p!(new: {s.expr()} {s.slice_desc()})
 			}
-			Expr::Var(v) => p!(pi: str(v)),
-			Expr::Arr(a) => {
-				p!(pi: str("["));
-				for (i, v) in a.iter().enumerate() {
-					if i != 0 {
-						p!(pi: str(", "));
-					}
-					p!(pi: {v})
+			Expr::ExprIndex(i) => {
+				p!(new: {i.expr()} str(".") {i.index()})
+			}
+			Expr::ExprIndexExpr(i) => p!(new: {i.base()} str("[") {i.index()} str("]")),
+			Expr::ExprApply(a) => {
+				let mut pi = p!(new: {a.expr()} {a.args_desc()});
+				if a.tailstrict_kw_token().is_some() {
+					p!(pi: str(" tailstrict"));
+				}
+				pi
+			}
+			Expr::ExprObjExtend(ex) => {
+				p!(new: {ex.lhs_expr()} str(" ") {ex.expr()})
+			}
+			Expr::ExprParened(p) => {
+				p!(new: str("(") {p.expr()} str(")"))
+			}
+			Expr::ExprIntrinsicThisFile(_) => p!(new: str("$intrinsicThisFile")),
+			Expr::ExprIntrinsicId(_) => p!(new: str("$intrinsicId")),
+			Expr::ExprIntrinsic(i) => p!(new: str("$intrinsic(") {i.name()} str(")")),
+			Expr::ExprString(s) => p!(new: {s.string()}),
+			Expr::ExprNumber(n) => p!(new: {n.number()}),
+			Expr::ExprArray(a) => {
+				let mut pi = p!(new: str("[") >i nl);
+				for el in a.exprs() {
+					p!(pi: {el} str(",") nl);
+				}
+				p!(pi: <i str("]"));
+				pi
+			}
+			Expr::ExprObject(o) => {
+				p!(new: str("{") >i nl {o.obj_body()} <i str("}"))
+			}
+			Expr::ExprArrayComp(arr) => {
+				let mut pi = p!(new: str("[") {arr.expr()});
+				for spec in arr.comp_specs() {
+					p!(pi: str(" ") {spec});
 				}
 				p!(pi: str("]"));
+				pi
 			}
-			Expr::ArrComp(_, _) => todo!(),
-			Expr::Obj(o) => {
-				p!(pi: {o});
+			Expr::ExprImport(v) => {
+				p!(new: {v.import_kind()} str(" ") {v.string()})
 			}
-			Expr::ObjExtend(a, b) => p!(pi: {a} str(" ") {b}),
-			Expr::Parened(v) => {
-				if let Expr::Parened(_) = &v.0 as &Expr {
-					p!(pi: {v})
-				} else {
-					p!(pi: str("(") {v} str(")"))
+			Expr::ExprVar(n) => p!(new: {n.name()}),
+			Expr::ExprLocal(l) => {
+				let mut pi = p!(new: str("local") >i nl);
+				for bind in l.binds() {
+					p!(pi: {bind} str(",") nl);
 				}
+				p!(pi: <i str(";") nl {l.expr()});
+				pi
 			}
-			Expr::UnaryOp(_, _) => todo!(),
-			Expr::BinaryOp(a, o, b) => {
-				p!(pi:
-					{a} str(" ") if(!matches!(&b.0 as &Expr, Expr::Obj(_)))({o} str(" ")) {b}
-				)
-			}
-			Expr::AssertExpr(_, _) => todo!(),
-			Expr::LocalExpr(s, v) => {
-				p!(pi:
-					str("local") nl >i
-				);
-				for spec in s.iter() {
-					p!(pi: {spec} str(";") nl)
+			Expr::ExprIfThenElse(ite) => {
+				let mut pi =
+					p!(new: str("if ") {ite.cond()} str(" then ") {ite.then().map(|t| t.expr())});
+				if ite.else_kw_token().is_some() || ite.else_().is_some() {
+					p!(pi: str(" else ") {ite.else_().map(|t| t.expr())})
 				}
-				p!(pi:
-					<i
-					{v}
-				);
+				pi
 			}
-			Expr::Import(i) => {
-				let v = i.to_str().unwrap();
-				p!(pi: str("import \"") str(v) str("\""));
-			}
-			Expr::ImportStr(_) => todo!(),
-			Expr::ErrorStmt(_) => todo!(),
-			Expr::Apply(f, a, t) => p!(pi:
-				{f} str("(") {a} str(")") if(*t)(str("tailstrict"))
-			),
-			Expr::Index(a, b) => p!(pi: {a} str("[") {b} str("]")),
-			Expr::Function(_, _) => todo!(),
-			Expr::Intrinsic(_) => todo!(),
-			Expr::IfElse {
-				cond,
-				cond_then,
-				cond_else,
-			} => p!(pi:
-				str("if ") {cond.0} str(" then") ifelse(cond_else.is_some())(
-					nl >i
-						{cond_then} nl
-					<i str("else") nl >i
-						{cond_else}
-					<i
-				)(str(" ") {cond_then})
-			),
-			Expr::Slice(v, d) => {
-				p!(pi:
-					{v}
-					str("[") {d.start} str(":") {d.end}
-					if(d.step.is_some())(
-						str(":")
-						{d.step}
-					)
-					str("]")
-				)
+			Expr::ExprFunction(f) => p!(new: str("function") {f.params_desc()} str(" ") {f.expr()}),
+			Expr::ExprAssert(a) => p!(new: {a.assertion()} str("; ") {a.expr()}),
+			Expr::ExprError(e) => p!(new: str("error ") {e.expr()}),
+			Expr::ExprLiteral(l) => {
+				p!(new: {l.literal()})
 			}
 		}
-		pi
 	}
 }
 
-impl Printable for LocExpr {
+impl Printable for SourceFile {
 	fn print(&self) -> PrintItems {
-		self.0.print()
+		assert!(self.expr().is_some());
+		self.expr().print()
 	}
 }
 
 fn main() {
-	let parsed = jrsonnet_parser::parse(
+	let (parsed, _errors) = jrsonnet_rowan_parser::parse(
 		r#"
 
 
@@ -314,10 +440,28 @@ fn main() {
 
 			 local f(x,y)=x+y;
 
+		local {a: [b, ..., c], d, ...e} = null;
+
+		local ass = assert false : false; false;
+
+		local fn = function(a, b, c = 3) 4;
+
+		local comp = [a for b in c if d == e];
+		local ocomp = {[k]: 1 for k in v};
+
+		local ? = skip;
+
+		local intr = $intrinsic(test);
+		local intrId = $intrinsicId;
+		local intrThisFile = $intrinsicThisFile;
+
+		local ie = a[expr];
+
+		local unary = !a;
 
 		local Template = {z: "foo"};
 
-		Template + {
+		{
 						local
 
 					h = 3,
@@ -347,21 +491,17 @@ fn main() {
 		  2
 
 		  else Template {}
-		}
+		} + Template
 
 
 "#,
-		&ParserSettings {
-			file_name: PathBuf::from("example").into(),
-		},
-	)
-	.unwrap();
+	);
+
+	// dbg!(errors);
+	dbg!(&parsed);
 
 	let o = dprint_core::formatting::format(
-		|| {
-			let print_items = parsed.print();
-			print_items
-		},
+		|| parsed.print(),
 		PrintOptions {
 			indent_width: 2,
 			max_width: 100,
